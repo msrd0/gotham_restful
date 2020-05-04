3de130e104055f52c220416920ff4ee1638adacf
Dominic <git@msrd0.de>
2020-05-04 20:30:15 +0200

    emit proper error message for `async fn read_all(state: &State)`

--- gotham_restful_derive/src/method.rs
+++ gotham_restful_derive/src/method.rs
@@ -133,6 +133,11 @@ enum MethodArgumentType
 
 impl MethodArgumentType
 {
+	fn is_state_ref(&self) -> bool
+	{
+		matches!(self, Self::StateRef | Self::StateMutRef)
+	}
+	
 	fn is_method_arg(&self) -> bool
 	{
 		matches!(self, Self::MethodArg(_))
@@ -368,6 +373,10 @@ pub fn expand_method(method : Method, mut attrs : AttributeArgs, fun : ItemFn) -
 	let mut state_block = quote!();
 	if fun_is_async
 	{
+		if let Some(arg) = args.iter().find(|arg| (*arg).ty.is_state_ref())
+		{
+			return Err(Error::new(arg.span(), "async fn must not take &State as an argument as State is not Sync"));
+		}
 		block = quote!(#block.await);
 	}
 	if is_no_content
