4c028cb62de7114b6ae4f6c1c5ebe19d50ef2391
Dominic <git@msrd0.de>
2021-04-16 01:41:19 +0200

    add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((rust-mode . ((lsp-rust-no-default-features . 't)
+			   (lsp-rust-features . ["full"]))))
