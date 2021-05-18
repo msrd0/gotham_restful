edac816f6abc3ac81c45076de4bf7b85da5d5c53
Dominic <git@msrd0.de>
2021-05-18 22:18:53 +0200

    fix redoc

---
+++ src/openapi/handler/webpack.config.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+
+const cfg: webpack.Configuration = {
+	entry: './script.ts',
+	module: {
+		rules: [
+			{
+				test: /\.ts/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.ts', '.js']
+	},
+	output: {
+		filename: 'script.min.js',
+		path: path.resolve(__dirname),
+		library: {
+			name: 'gotham_restful',
+			type: 'var'
+		}
+	}
+};
+
+export default cfg;
