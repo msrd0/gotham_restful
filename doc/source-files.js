32f0ae8d6d49ed9c7007d0e26a180099ef46ac07
msrd0 <msrd0@users.noreply.github.com>
2022-07-04 08:42:25 +0000

    GitHub Pages for 776e5377ba8bab05c7ee627e428fe1f5c9f4ec81

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["gotham_restful"] = {"name":"","dirs":[{"name":"openapi","dirs":[{"name":"handler","files":["mod.rs"]}],"files":["builder.rs","mod.rs","operation.rs","router.rs"]},{"name":"response","files":["auth_result.rs","mod.rs","no_content.rs","raw.rs","redirect.rs","result.rs","success.rs"]}],"files":["auth.rs","cors.rs","endpoint.rs","lib.rs","routing.rs","types.rs"]};
 createSourceSidebar();
