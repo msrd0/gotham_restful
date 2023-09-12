7d7fe4f3a8277ccb635a6652063ef25f9fc44e10
msrd0 <msrd0@users.noreply.github.com>
2023-09-12 22:04:59 +0000

    GitHub Pages for 82134597c1b29d0a32d09e09b336692218fd9d92

--- doc/implementors/gotham/router/response/extender/trait.StaticResponseExtender.js
+++ doc/implementors/gotham/router/response/extender/trait.StaticResponseExtender.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gotham_restful":[["impl <a class=\"trait\" href=\"https://docs.rs/gotham/0.7.1/gotham/router/response/extender/trait.StaticResponseExtender.html\" title=\"trait gotham::router::response::extender::StaticResponseExtender\">StaticResponseExtender</a> for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]
+"gotham_restful":[["impl <a class=\"trait\" href=\"https://docs.rs/gotham/0.7.2/gotham/router/response/extender/trait.StaticResponseExtender.html\" title=\"trait gotham::router::response::extender::StaticResponseExtender\">StaticResponseExtender</a> for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
