fe2e232e3765a96233c64136f630f7d1c5cb6533
msrd0 <msrd0@users.noreply.github.com>
2023-10-09 14:22:47 +0000

    GitHub Pages for a987d2aecccf8030ed4f7c6cc2e5a4fa1560a39b

--- doc/src-files.js
+++ doc/src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "gotham_restful":["",[["openapi",[["handler",[],["mod.rs"]]],["builder.rs","mod.rs","operation.rs","router.rs"]],["response",[],["auth_result.rs","mod.rs","no_content.rs","raw.rs","redirect.rs","result.rs","success.rs"]]],["auth.rs","cors.rs","endpoint.rs","lib.rs","routing.rs","types.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
