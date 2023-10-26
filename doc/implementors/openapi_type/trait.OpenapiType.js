0e601dce4646468e232419ea454a30acbb110a6a
msrd0 <msrd0@users.noreply.github.com>
2023-10-26 11:27:43 +0000

    GitHub Pages for bbfb47a01fe717576e64fd9cf8addd1734316c8e

--- doc/implementors/openapi_type/trait.OpenapiType.js
+++ doc/implementors/openapi_type/trait.OpenapiType.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gotham_restful":[["impl OpenapiType for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"],["impl&lt;T&gt; OpenapiType for <a class=\"struct\" href=\"gotham_restful/struct.Raw.html\" title=\"struct gotham_restful::Raw\">Raw</a>&lt;T&gt;"]]
+"gotham_restful":[["impl&lt;T&gt; OpenapiType for <a class=\"struct\" href=\"gotham_restful/struct.Raw.html\" title=\"struct gotham_restful::Raw\">Raw</a>&lt;T&gt;"],["impl OpenapiType for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
