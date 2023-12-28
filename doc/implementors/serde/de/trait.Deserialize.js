896a46917840fb72df112c05fd53548c8bca62c0
msrd0 <msrd0@users.noreply.github.com>
2023-12-28 20:42:29 +0000

    GitHub Pages for 59d30dd26be0047a815b99e919d87b4881cfac08

--- doc/implementors/serde/de/trait.Deserialize.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"gotham_restful":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.190/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ doc/trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"gotham_restful":[["impl&lt;T, U&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;U&gt; for <a class=\"struct\" href=\"gotham_restful/struct.Raw.html\" title=\"struct gotham_restful::Raw\">Raw</a>&lt;T&gt;<span class=\"where fmt-newline\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;U&gt;,</span>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
