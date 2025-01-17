18f68f8fdb9eae0f85243336b0e134bc0639f7b2
msrd0 <7853372+msrd0@users.noreply.github.com>
2025-01-17 07:56:42 +0000

    GitHub Pages for 06584ed9fb672014bc1b1a750193105ba47a188a

--- doc/trait.impl/serde/de/trait.Deserialize.js
+++ doc/trait.impl/serde/de/trait.Deserialize.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["gotham_restful",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]],["gotham_restful",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.215/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]]]);
+    var implementors = Object.fromEntries([["gotham_restful",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[335,336]}
+//{"start":57,"fragment_lengths":[335]}
