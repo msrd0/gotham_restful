15e8e010d8537ec2cc34079a30e19c9c4d1a29da
msrd0 <7853372+msrd0@users.noreply.github.com>
2025-02-08 08:27:32 +0000

    GitHub Pages for 9d9553942ed2f19c101d6465df0bb55510f2e308

--- doc/trait.impl/core/marker/trait.Copy.js
+++ doc/trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["gotham_restful",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]]]);
+    var implementors = Object.fromEntries([["gotham_restful",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"gotham_restful/struct.NoopExtractor.html\" title=\"struct gotham_restful::NoopExtractor\">NoopExtractor</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
