03a3353198725bc2960b3a56ad22ff15c8a4e04f
msrd0 <7853372+msrd0@users.noreply.github.com>
2025-08-16 19:13:12 +0000

    GitHub Pages for 27d992b453f6af086a8ac993e0a6eaf51b5eb931

--- doc/src-files.js
+++ doc/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["gotham_restful",["",[["openapi",[["handler",[],["mod.rs"]]],["builder.rs","mod.rs","operation.rs","router.rs"]],["response",[],["auth_result.rs","mod.rs","no_content.rs","raw.rs","redirect.rs","result.rs","success.rs"]]],["auth.rs","cors.rs","endpoint.rs","lib.rs","routing.rs","types.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[293]}
+createSrcSidebar('[["gotham_restful",["",[["openapi",[["handler",[],["mod.rs"]]],["builder.rs","mod.rs","operation.rs","router.rs"]],["response",[],["auth_result.rs","mod.rs","no_content.rs","raw.rs","redirect.rs","result.rs","success.rs"]]],["auth.rs","cors.rs","endpoint.rs","lib.rs","routing.rs","types.rs"]]]]');
+//{"start":19,"fragment_lengths":[293]}
