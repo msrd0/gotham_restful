39cb7158237adf0bc73ea0e9bdb35d13190597aa
msrd0 <7853372+msrd0@users.noreply.github.com>
2026-05-30 17:59:28 +0000

    GitHub Pages for 8e2f90f36ee7072725713a06d830abe424940819

--- doc/trait.impl/core/convert/trait.AsMut.js
+++ doc/trait.impl/core/convert/trait.AsMut.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["gotham_restful",[["impl&lt;T, U&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;U&gt; for <a class=\"struct\" href=\"gotham_restful/struct.Raw.html\" title=\"struct gotham_restful::Raw\">Raw</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;U&gt;,</div>",0]]]]);
+    const implementors = Object.fromEntries([["gotham_restful",[["impl&lt;T, U&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;U&gt; for <a class=\"struct\" href=\"gotham_restful/struct.Raw.html\" title=\"struct gotham_restful::Raw\">Raw</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/convert/trait.AsMut.html\" title=\"trait core::convert::AsMut\">AsMut</a>&lt;U&gt;,</div>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
